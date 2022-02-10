496ab395248b881f52b66684c06c07b2ef038fe6
MunnY <mahmudaaktermunny4@gmail.com>
2022-02-10 16:28:21 +0600

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const isbnInput = document.querySelector("#isbn");
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const isbn = document.querySelector('#isbn');
+const btn = document.querySelector('.btn');
+const bookList = document.querySelector('#book-list');
 
-const button = document.querySelector(".btn");
-const bookList = document.querySelector("#book-list");
-
-
-button.addEventListener("click", function() {
-    if (titleInput.value == "" && authorInput.value == "" && isbnInput.value == "") {
-        alert("Enter Any Input");
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (title.value == '' && author.value == '' && isbn.value == '') {
+        alert('Enter Your Data');
     } else {
+        const newRow = document.createElement('tr');
 
+
+        //create a new title
+
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML = author.value;
+        newRow.appendChild(newAuthor);
+
+        const newIsbn = document.createElement('th');
+        newIsbn.innerHTML = isbn.value;
+        newRow.appendChild(newIsbn);
+
+
+        bookList.appendChild(newRow);
     }
-})
+});
